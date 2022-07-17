3fae09313fb623d8722d7c4fce2b9494dca6fd88
Saad Ahmed <saad18910@hotmail.com>
2022-07-17 19:38:13 +0500

    Added Stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -107,24 +107,110 @@ export class AppComponent {
 
     // this.miniMaxSum(arr);
 
-    const integers = [34, 95, 34, 64, 45, 95, 16, 80, 80, 75, 3, 25, 75, 25, 31, 3, 64, 16, 31];
-    console.log('Loner:', this.lonelyinteger(integers));
+    // const integers = [34, 95, 34, 64, 45, 95, 16, 80, 80, 75, 3, 25, 75, 25, 31, 3, 64, 16, 31];
+    //console.log('Loner:', this.lonelyinteger(integers));
+
+    // const matrix: number[][] = [[11, 2, 4], [4, 5, 6], [10, 8, -12]];
+    // console.log(this.diagonalDifference(matrix));
+
+    // const sortValues = [63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91,
+    //   39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34,
+    //   68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60,
+    //   65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33];
+
+    // console.log({ sortValues, sorted: this.countingSort(sortValues) });
+
+    // const text = 'We promptly judged antique ivory buckles for the next prize';
+    // console.log(this.pangrams(text));
+
+    console.log(this.buildArray([1, 3], 10));
+  }
+
+
+  buildArray(target: number[], n: number): string[] {
+
+    const actions: string[] = [];
+
+    for (let i = 1, x = 0; i <= n; ++i) {
+      if (target[x] === i) {
+        actions.push("Push");
+
+        if (target[target.length - 1] === i) {
+          i = n;
+        }
+        else {
+          x++;
+        }
+
+
+      } else {
+        actions.push("Push");
+        actions.push("Pop");
+      }
+
+    }
+
+    return actions;
+
+  };
+
+  pangrams(s: string): string {
+    // Write your code here
+
+    let isPangram = true;
+    const newText = Array.from(new Set(s.replace(/ /g, '').toLowerCase().split(''))).sort();
+    const alphabets = Array(26).fill(97).map((value, i) => String.fromCharCode(value + i));
+
+    for (let i = 0; i < alphabets.length && isPangram; ++i) {
+      if (alphabets[i] !== newText[i]) {
+        isPangram = false;
+      }
+
+    }
+
+    return isPangram ? 'pangram' : 'not pangram';
+  }
+
+
+  countingSort(arr: number[]): number[] {
+    // Write your code here
+    const countArr = Array(100).fill(0);
+
+    arr.forEach((val, i) => countArr[val]++);
+
+    return countArr;
+  }
+
+  diagonalDifference(arr: number[][]): number {
+    // Write your code here
+    let leftSum = 0;
+    let rightSum = 0;
+
+
+    for (let i = 0; i < arr.length; ++i) {
+      leftSum += arr[i][i];
+      rightSum += arr[i][arr.length - 1 - i];
+      console.log({ leftSum, rightSum, i, r: arr.length - 1 - i })
+    }
+
+    return Math.abs(leftSum - rightSum);
+
   }
 
   lonelyinteger(a: number[]): number {
+    const object: any = {};
 
-    const b = a.sort((j, k) => j - k);
-    console.log(b)
-    let loner = b[0];
+    for (let i = 0; i < a.length; ++i) {
+      const index = a[i];
 
-    for (let i = 1; i < b.length; ++i) {
-      if (b[i] !== loner) {
-        loner = b[i];
+      if (!(!!object[index] || object[index] >= 0)) {
+        object[index] = 1;
       } else {
-        loner = -1;
+        object[index]++;
       }
     }
-    return loner;
+
+    return Number(Object.keys(object).find(key => object[key] === 1));
   }
 
   matchingStrings(strings: string[], queries: string[]): number[] {
